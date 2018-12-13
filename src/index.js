243bd143573993b1866bbd7726450f63c786f955
euZebe <euzebe@axiome.me>
2018-12-13 14:28:21 +0100

    clean // TODO

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ store.subscribe(() => {
   console.table(store.getState());
 });
 
-// TODO 7: add subscriber displaying only filtered tasks
 
-// TODO 4
 store.dispatch({ type: "TASK_ADD", title: "actions", id: 1 });
 store.dispatch({ type: "TASK_ADD", title: "reducer", id: 2 });
 store.dispatch({ type: "TASK_ADD", title: "combine reducers", id: 3 });
 store.dispatch({ type: "TASK_ADD", title: "subscribers", id: 4 });
-// TODO 3
+
 store.dispatch({ type: "TASK_STATUS_TOGGLE", id: 3 });
 
-// TODO 6
 store.dispatch({ type: "FILTER_CHANGE", filter: "all" });
